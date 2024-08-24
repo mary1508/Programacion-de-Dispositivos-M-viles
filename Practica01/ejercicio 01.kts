9893d59f1eb45e1906daaa97d09c70f24692608b
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-08-24 00:42:19 -0500

    realizando la configuración inicial y lectura de Datos puntuación y salario

---
+++ Practica01/ejercicio 01.kts
@@ -0,0 +1,10 @@
+fun main() {
+    println("Ingrese su puntuación: ")
+    val puntuacion = readLine()?.toInt()
+
+    println("Ingrese su salario mensual: ")
+    val salario = readLine()?.toInt()
+
+    println("Puntuación ingresada: $puntuacion")
+    println("Salario ingresado: $salario")
+}

9999a154cf0efdaaa97162f99ec199da2caf0b28
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-08-25 22:44:55 -0500

    implemento las funciones de suma y resta como primeras opciones para seleccionar en el menu, se hara el calculo de acuerdo a lo que el usuario ingrese.

--- Practica01/ejercicio 03.kt
+++ Practica01/ejercicio 03.kt
@@ -1,4 +1,10 @@
-
+/* Descripcion del problema: Realizar una calculadora  básica
+suma, resta, multiplicación, división. Debe imprimir
+un menú con las opciones incluyendo una opción salir.
+Autor: Marycielo Bedoya Pinto
+Fecha de creación: 25/08/2024
+Fecha de ultima modificación:
+ */
 fun mostrarMenu() {
     while (true) {
 
@@ -26,4 +32,23 @@ fun mostrarMenu() {
             else -> println("Opción inválida. Por favor, elige una opción válida.")
         }
     }
+}
+fun realizarSuma() {
+    println("Ingresa el primer número:")
+    val num1 = readln().toDouble()
+    println("Ingresa el segundo número:")
+    val num2 = readln().toDouble()
+
+    // Aqui hacemos el calculo de la suma
+    val resultado = num1 + num2
+    println("Resultado de la suma: $resultado")
+}
+
+fun realizarResta() {
+    println("Ingresa el primer número:")
+    val num1 = readln().toDouble()
+    println("Ingresa el segundo número:")
+    val num2 = readln().toDouble()
+    val resultado = num2 - num1
+    println("Resultado de la resta: $resultado")
 }

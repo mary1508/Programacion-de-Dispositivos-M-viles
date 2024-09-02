c94d1a8a6fba2f65d4eb5e239cd44e62e6b74526
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-09-02 00:48:42 -0500

    se ha creado la clase abstracta Material para encapsular propiedades comunes como título, autor y año de publicación, y definir el método abstracto mostrarDetalles() para su personalización en subclases específicas

---
+++ Practica02/ejercicio 04.kt
@@ -0,0 +1,40 @@
+// Descripción: Sistema de Gestión de Biblioteca con clases para materiales y usuarios.
+// Autor: Marycielo Bedoya Pinto
+// Fecha creación: 02/09/2024
+// Fecha última modificación:
+
+abstract class Material(
+    val titulo: String,
+    val autor: String,
+    val anioPublicacion: Int
+) {
+    abstract fun mostrarDetalles()
+}
+
+class Libro(
+    titulo: String,
+    autor: String,
+    anioPublicacion: Int,
+    val genero: String,
+    val numeroPaginas: Int
+) : Material(titulo, autor, anioPublicacion) {
+    override fun mostrarDetalles() {
+        println("Libro: $titulo, Autor: $autor, Año: $anioPublicacion, Género: $genero, Páginas: $numeroPaginas")
+    }
+}
+
+class Revista(
+    titulo: String,
+    autor: String,
+    anioPublicacion: Int,
+    val issn: String,
+    val volumen: Int,
+    val numero: Int,
+    val editorial: String
+) : Material(titulo, autor, anioPublicacion) {
+    override fun mostrarDetalles() {
+        println("Revista: $titulo, Autor: $autor, Año: $anioPublicacion, ISSN: $issn, Volumen: $volumen, Número: $numero, Editorial: $editorial")
+    }
+}
+
+data class Usuario(val nombre: String, val apellido: String, val edad: Int)

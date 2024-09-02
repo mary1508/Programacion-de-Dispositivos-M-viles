aaffb4a626cef6a6d23d0d50e252db71d418c7fa
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-09-02 00:30:40 -0500

    hacemos lo mismo con las clases circulo y retangulo Agregamos a la funcion main el circulo y rectagulo par ahora si  confirmar funcionamiento

--- Practica02/ejercicio 03.kt
+++ Practica02/ejercicio 03.kt
@@ -6,6 +6,7 @@
 abstract class Shape {
     abstract fun calcularArea(): Double
     abstract fun calcularPerimetro(): Double
+
     fun imprimirResultados() {
         println("Área: ${calcularArea()}")
         println("Perímetro: ${calcularPerimetro()}")
@@ -21,7 +22,34 @@ class Cuadrado(private val lado: Double) : Shape() {
         return 4 * lado
     }
 }
+
+class Circulo(private val radio: Double) : Shape() {
+    override fun calcularArea(): Double {
+        return Math.PI * radio * radio
+    }
+
+    override fun calcularPerimetro(): Double {
+        return 2 * Math.PI * radio
+    }
+}
+
+class Rectangulo(private val largo: Double, private val ancho: Double) : Shape() {
+    override fun calcularArea(): Double {
+        return largo * ancho
+    }
+
+    override fun calcularPerimetro(): Double {
+        return 2 * (largo + ancho)
+    }
+}
+
 fun main() {
     val cuadrado = Cuadrado(4.0)
     cuadrado.imprimirResultados()
+
+    val circulo = Circulo(3.0)
+    circulo.imprimirResultados()
+
+    val rectangulo = Rectangulo(5.0, 3.0)
+    rectangulo.imprimirResultados()
 }

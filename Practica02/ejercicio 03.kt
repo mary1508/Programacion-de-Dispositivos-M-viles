1c3a330e6435f539e0b64dae4809952c954fec08
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-09-02 00:16:00 -0500

    agregamos la clase abstracta shape y las funciones abs calcular area y calcular perimetro, las dos como tipo double.

---
+++ Practica02/ejercicio 03.kt
@@ -0,0 +1,10 @@
+// Descripción: Clase abstracta para calcular área y perímetro de figuras.
+// Autor: Marycielo Bedoya Pinto
+// Fecha creación: 02/09/2024
+// Fecha última modificación:
+
+abstract class Shape {
+    abstract fun calcularArea(): Double
+    abstract fun calcularPerimetro(): Double
+
+}

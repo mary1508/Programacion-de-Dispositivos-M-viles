ea6e5d4346caae2fcb75e1fd823abfbc84295d88
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-09-01 23:16:29 -0500

    creando descripcion, autor y fechas, y agregando clase para cuenta bancaria.

--- Practica02/ejercicio 01.kt
+++ Practica02/ejercicio 01.kt
@@ -1,1 +1,12 @@
-uhu
+// Descripción: Clase para manejar una cuenta bancaria con saldo y límite de retiro.
+// Autor: Marycielo Bedoya Pinto
+// Fecha creación: 01/09/2024
+// Fecha última modificación:
+
+class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double) {
+
+    // Obtenemos el saldo actual
+    fun getSaldo(): Double {
+        return saldo
+    }
+}

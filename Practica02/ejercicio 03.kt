e6664f425e244a3aac740b97f06a5ea1388ddb90
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-09-02 00:23:39 -0500

    agregamos la clase cuadrado junto con las funciones de sobre escritura de calcularArea y perimetro , ademas que a shape se le agrego una funcion para imprimir resultados para cuando se pida calcular area se llame a la funcion calcularArea y de igusl manera para perimetro. Agregamos la funcion main para probar la clase cuadrado.

--- Practica02/ejercicio 03.kt
+++ Practica02/ejercicio 03.kt
@@ -6,5 +6,22 @@
 abstract class Shape {
     abstract fun calcularArea(): Double
     abstract fun calcularPerimetro(): Double
+    fun imprimirResultados() {
+        println("Área: ${calcularArea()}")
+        println("Perímetro: ${calcularPerimetro()}")
+    }
+}
 
+class Cuadrado(private val lado: Double) : Shape() {
+    override fun calcularArea(): Double {
+        return lado * lado
+    }
+
+    override fun calcularPerimetro(): Double {
+        return 4 * lado
+    }
+}
+fun main() {
+    val cuadrado = Cuadrado(4.0)
+    cuadrado.imprimirResultados()
 }

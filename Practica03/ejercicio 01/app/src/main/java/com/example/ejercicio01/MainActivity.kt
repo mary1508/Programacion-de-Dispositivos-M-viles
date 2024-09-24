928f8a2800080ef5b3a2e14fd540c916bd37b021
MARYCIELO <mbedoyap@ulasalle.edu.pe>
2024-09-23 23:51:12 -0500

    Implementando main activity para la selección de imagen con el spinner y con el botón siguiente y tambien, implementando funciones onsaveinstancestate y onrestoreinstancestate

--- Practica03/ejercicio 01/app/src/main/java/com/example/ejercicio01/MainActivity.kt
+++ Practica03/ejercicio 01/app/src/main/java/com/example/ejercicio01/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.ejercicio01
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+// Descripción: Actividad principal que permite seleccionar una imagen y navegar a la siguiente actividad.
+// Autor: Marycielo Bedoya Pinto
+// Fecha de creación: 2024-09-20
+// Fecha de última modificación: 2024-09-20
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var spinner: Spinner
+    lateinit var selectedImage: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        spinner = findViewById(R.id.spinner_images)
+        val images = arrayOf("Image1", "Image2", "Image3")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, images)
+        spinner.adapter = adapter
+
+        // Restaurar la selección del spinner si esta disponible
+        selectedImage = savedInstanceState?.getString("selectedImage") ?: images[0]
+
+        val btnNext: Button = findViewById(R.id.btn_next)
+        btnNext.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("selectedImage", spinner.selectedItem.toString())
+            startActivity(intent)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("selectedImage", spinner.selectedItem.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        selectedImage = savedInstanceState.getString("selectedImage") ?: "Image1"
+        spinner.setSelection((spinner.adapter as ArrayAdapter<String>).getPosition(selectedImage))
     }
 }
